9b85314eb73470ed3310572f578c60cd43df655e
priyankareddy135 <priyankareddy135@users.noreply.github.com>
2016-06-16 15:37:44 +0530

    updated with code for more findings(reducer)

--- reducer.py
+++ reducer.py
@@ -1,64 +1,62 @@
-#!/usr/bin/python
-
 import operator
 import sys
 import pandas as pd
+from twitter import *
+from datetime import datetime
 
 def Main():
-  data = read_mapper_output()
-  uqueries = find_unique_queries(data)
-  max_query(uqueries)
-  min_query(uqueries)
+      data = read_mapper_output()
+      print(data)
+      tweetrate(data)
+      relationship_between_two_users(data)
+      time = timeline(data)
+      print(data)
+      #retweet = 
+      retweets(data)
+      #retweeter_id(retweet)
 def read_mapper_output():
-  data = []
-  for line in sys.stdin:
-    data.append(line)
-  return data
+      data = []
+      for line in sys.stdin:
+        line.split("\t")
+        data.append(line)
+      return data
+def tweetrate(data):
+    mean = data[0].split("\t")
+    length = data[1].split("\t")
+    m = float(mean[1])
+    length1 = float(length[1])
+    print "finding5###tweet rate ::",m/length1
 
-def find_unique_queries(data):
-  uqueries = {}
-  for row in data:
-    key, val = row.split("\t")
-    val = int(val)
-    if not uqueries.has_key(key):
-      uqueries[key] = 0
-    uqueries[key] += val 
-  for key, val in uqueries.iteritems():
-    print("{0}\t{1}".format(key, val))
-  return uqueries
+def relationship_between_two_users(data):
+    source = data[2].split("\t")
+    target = data[3].split("\t")
+    follows = data[4].split("\t")
+    following = data[5].split("\t")
+    print "finding6#### %s following %s: %s" % (source[1], target[1], follows[1])
+    print "finding6#### %s following %s: %s" % (target[1], source[1], following[1])
 
-def max_query(uqueries):
-  print max(uqueries.iteritems(), key = operator.itemgetter(1))[0]
+def timeline(data):
+    results = data[6].split("\t")
+    results.append(data[7].strip("\t"))
+    results.append(data[8].split("\t"))
+    results.append(data[9].split("\t"))
+    results.append(data[10].split("\t"))
+    results.append(data[11].split("\t"))
+    results.append(data[12].split("\t"))
+    print results
+    print "ended"
 
-def min_query(uqueries):
-  print min(uqueries.iteritems(), key = operator.itemgetter(1))[0]
-
-
+def retweets(data):
+    print "hello"
+    tweet = data[15].split("\t")
+    print tweet
 Main()
-
-
-"""
-qdata = readdata_query()
-def function1():
-        for i in counts:
-		list.append(qdata[i])
-	maxval = max(qdata)
-	for query,qdata in qdata.iteritems():
-		if qdata == maxval:
-			return query
-def function2():
-	for i in qdata:
-                list.append(qdata[i])
-        minval = min(qdata)
-        for query,qdata in qdata.iteritems():
-                if qdata == minval:
-                        return query
-def function3():
-        totalnum = len(qdata)
-        return totalnum
-def function4():
-	
-	
-"""
-	
     
+#def retweeter_id(retweet):
+ #    retweets = twitter.statuses.retweets._id(_id = status["id"])
+     #count = 0;
+  #   for retweet in retweets:
+        #count += 1
+   #     print " - retweeted by %s" % (retweet["user"]["screen_name"])
+
+

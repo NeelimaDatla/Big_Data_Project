6e0c1c8122a15f992c4b4de1634f7aa93de52df9
priyankareddy135 <priyankareddy135@users.noreply.github.com>
2016-06-16 15:37:03 +0530

    updated with code for more findings

--- mapper.py
+++ mapper.py
@@ -1,37 +1,82 @@
-#!/usr/bin/python
-
+from twitter import *
+from datetime import datetime
 import sys
-import csv
-import pandas as pd
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-def Main():
-  data = read_data_from_csv()
-  qrylist = find_unique_queries(data)
-  count_unique_queries(qrylist)
-
-def read_data_from_csv():
-  df = pd.read_csv(sys.stdin)
-  return df
-
-def find_unique_queries(df):
-  querylist = list(df.Query)
-  for qry in querylist:
-    print("{0}\t{1}".format(qry, 1))
-  return querylist
+def main():
+    tweetrate()
+    realtonship_between_twousers()
+    retweets()
+    timeline()
+#code for finding tweet rate
+def tweetrate():
+     created_at_format = '%a %b %d %H:%M:%S +0000 %Y'
+     config = {}
+     execfile("config.py", config)
+     twitter = Twitter(auth = OAuth(config["atoken"], config["asecret"],
+         config["ckey"], config["csecret"]))
   
-Main()
+     terms = "pink elephants"
+     query = twitter.search.tweets(q = terms)
+     results = query["statuses"]
+     first_timestamp = datetime.strptime(results[0]["created_at"], created_at_format)
+     last_timestamp = datetime.strptime(results[-1]["created_at"], created_at_format)
+     mean = (first_timestamp - last_timestamp).total_seconds()
+     length = len(results)
+     #print("{0}\t{1}".format("firsttimestamp",first_timestamp))
+     print("{0}\t{1}".format("finding5###mean",mean))
+     print("{0}\t{1}".format("finding5####length",len(results)))
 
-"""
-def readdata_query():
-        print(querylist)
-   	from collections import Counter
-	counts = Counter(querylist)
-	print(counts)
-	return counts
-#def readcsvdata():
-#	df = pd.read_csv(sys.stdin)
- 
-readdata_query()
+####code for relationship between two users
+def  realtonship_between_twousers():
+     config = {}
+     execfile("config.py", config)
+     twitter = Twitter(auth = OAuth(config["atoken"], config["asecret"], config["ckey"], config["csecret"]))
+     source = "modi"
+     target = "nstomar"
+     result = twitter.friendships.show(source_screen_name = source,target_screen_name = target)
+     following = result["relationship"]["target"]["following"]
+     follows   = result["relationship"]["target"]["followed_by"]
+     print("{0}\t{1}".format("finding6###souce",source))
+     print("{0}\t{1}".format("finding6###target",target))
+     print("{0}\t{1}".format("finding6###follows",follows))
+     print("{0}\t{1}".format("finding6###following",following))
 
-"""
+#####code for finding retweets
+
+def retweets():
+     user = "jaspritb1"
+     config = {}
+     execfile("config.py", config)
+     twitter = Twitter(auth = OAuth(config["atoken"], config["asecret"], config["ckey"], config["csecret"]))
+     results = twitter.statuses.user_timeline(screen_name = user)
+     for status in results:
+        #print("{0}\t{1}".format("finding7###status",status["text"]))
+        # print "Finding###status %s" % (status["text"])
+         retweets = twitter.statuses.retweets._id(_id = status["id"])
+         #count = 0;
+         for retweet in retweets:
+            #count += 1
+            #print " - retweeted by %s" % (retweet["user"]["screen_name"])
+            print("{0}\t{1}".format("finding7###status",(retweet["user"]["screen_name"])))
+
+
+def timeline():
+    config = {}
+    execfile("config.py", config)
+    twitter = Twitter(auth = OAuth(config["atoken"], config["asecret"], config["ckey"], config["csecret"]))
+    user = "modi"
+    results = twitter.statuses.user_timeline(screen_name = user)
+    #print("{0}\t{1}".format("finding8###result",results))
+    for status in results:
+        data = (status["created_at"], status["text"])
+        #print data
+        print("{0}\t{1}".format("finding8###data",data))
+
+
+main()
+    
+     
+
